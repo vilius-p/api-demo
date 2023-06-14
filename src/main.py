2fe035b9376cc2a1784c465df1a0fb481c79095c
vilius-p <vilius@hp.home>
2023-06-14 18:08:11 +0300

    add prometheus

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from prometheus_client import start_http_server, Summary
 
 import requests
 
 app = FastAPI()
+request_time = Summary("request_processing_seconds", "Time spent processing request")
 
 
 @app.get("/hello")
@@ -19,8 +21,19 @@ async def get_quote():
     else:
         return {"error": "Failed to fetch the quote"}
 
+@app.get("/health")
+def get_health():
+    return {"status": "healthy"}
+
+@app.get("/metrics")
+def get_metrics():
+    # Expose Prometheus metrics
+    return Response(prometheus_client.generate_latest(), media_type="text/plain")
+
 
 if __name__ == "__main__":
     import uvicorn
+
+    start_http_server(8001)
 
     uvicorn.run(app, host="0.0.0.0", port=8000)
