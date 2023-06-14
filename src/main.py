2936033e93a861c88fdfee0a6699c9dbf8282a7c
vilius-p <vilius@hp.home>
2023-06-14 18:37:15 +0300

    add collectorReg

--- src/main.py
+++ src/main.py
@@ -1,18 +1,36 @@
 from fastapi import FastAPI, Response
-from prometheus_client import start_http_server, Summary
-
+from prometheus_client import (
+    Counter,
+    Histogram,
+    start_http_server,
+    generate_latest,
+    CollectorRegistry,
+)
 import requests
 
+metrics_reg = CollectorRegistry()
+num_q = CollectorRegistry(
+    "api_num_queries", "counts number of requests sent to API", ["endpoint"]
+)
+num_err = CollectorRegistry("api_num_errors", "counts number of errors occurred")
+latency = Histogram("api_latency", "latency calculator")
+# num_q = Counter('api_num_queries','counts number of requests sent to API', ['endpoint'])
+# num_err = Counter('api_num_errors','counts number of errors occurred')
+# latency = Histogram('api_latency', 'latency calculator')
+
 app = FastAPI()
-request_time = Summary("request_processing_seconds", "Time spent processing request")
 
 
 @app.get("/hello")
+@latency.time()
+@num_err.count_exceptions()
 async def hello():
     return "Hello Telia"
 
 
 @app.get("/quote")
+@latency.time()
+@num_err.count_exceptions()
 async def get_quote():
     # response = requests.get("https://zenquotes.io/api/today")
     response = requests.get("https://zenquotes.io/api/random")
@@ -23,15 +41,19 @@ async def get_quote():
 
 
 @app.get("/health")
+@latency.time()
+@num_err.count_exceptions()
 def get_health():
     return {"status": "healthy"}
 
 
 @app.get("/metrics")
+@latency.time()
+@num_err.count_exceptions()
 def get_metrics():
     # Expose Prometheus metrics
-    return Response(prometheus_client.generate_latest(), media_type="text/plain")
-
+    # return Response(prometheus_client.generate_latest(), media_type="text/plain")
+    return generate_latest(metrics_reg)
 
 if __name__ == "__main__":
     import uvicorn
